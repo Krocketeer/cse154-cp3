44c922d6ba10987ea66900432f18c530f5f94a9d
Krocketeer <Krocketeer@github.com>
2022-02-08 01:23:15 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Kenny "Ackerson" Le
+ * CSE 154 AF, Winter 2022
+ * TA: Ludvig Liljenberg, Marina Wooden
+ * 2/26/22
+ * Creative Project #3
+ * Description:
+ */
+
+'use strict';
+(function() {
+  window.addEventListener('load', init);
+
+  function init() {
+    console.log('Hello world!');
+  }
+})();
